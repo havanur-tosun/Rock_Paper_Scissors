1e9afc902f618221b3760fc7af254cf6ece166c1
Havanur <havanurtosun0618@gmail.comhavanurtosun0618@gmail.com>
2021-07-17 13:39:15 -0400

    commit

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 const selectionButtons = document.querySelectorAll("[data-selection]");
+const finalColumn = document.querySelector("[data-final-column]");
+const computerScoreSpan = document.querySelector("[data-computer-score]");
+const yourScoreSpan = document.querySelector("[data-your-score]");
 const SELECTIONS = [
     {
         name: 'rock',
@@ -14,20 +17,59 @@ const SELECTIONS = [
         name: 'scissors',
         emoji: '✌',
         beats: 'paper'
-    },
+    }
 ]
 
 
-selectionButtons.forEach(selectionButton =>{
-    selectionButton.addEventListener("click", e =>{
+selectionButtons.forEach(selectionButton => {
+    selectionButton.addEventListener('click', e => {
         const selectionName = selectionButton.dataset.selection
         const selection = SELECTIONS.find(selection => selection.name === selectionName)
-        makeSelection(selectionName)
+        makeSelection(selection)
     })
-});
+})
 
 
 function makeSelection(selection) {
-    console.log(selection)
+    const computerSelection = randomSelection()
+    const yourWinner = isWinner(selection, computerSelection)
+    const computerWinner = isWinner(computerSelection, selection)
+    // console.log(computerSelection)
+
+    addSelectionResult(computerSelection, computerWinner)
+    addSelectionResult(selection, yourWinner)
+
+
+    if (yourWinner) incrementScore(yourScoreSpan)
+    if (computerWinner) incrementScore(computerScoreSpan)
+
 }
+
+
+function incrementScore(scoreSpan) {
+    scoreSpan.innerText = parseInt(scoreSpan.innerText) + 1
+}
+
+
+
+function addSelectionResult(selection, winner) {
+    const div = document.createElement('div')
+    div.innerText = selection.emoji
+    div.classList.add("result-selection")
+    if (winner) div.classList.add("winner")
+    finalColumn.after(div)
+}
+
+
+function isWinner (selection, opponentSelection) {
+    return selection.beats === opponentSelection.name
+}
+
+
+
+
+function randomSelection() {
+    const ramdomIndex = Math.floor(Math.random() * SELECTIONS.length)
+    return SELECTIONS[ramdomIndex]
+};
 
